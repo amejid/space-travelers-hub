496c65133807da7b98935775d8d33705c3e82e89
stephnna <stephennnabuike@gmail.com>
2022-08-23 23:33:29 +0100

    Implement leave mission and change mission badge

--- src/components/missions/DisplayMissions.js
+++ src/components/missions/DisplayMissions.js
@@ -4,18 +4,28 @@ import { becomeMember } from '../../redux/missions/missions';
 
 const DisplayMission = (props) => {
   const {
-    id, name, description,
+    id, name, description, reserved,
   } = props;
   const dispatch = useDispatch();
   const joinMission = () => dispatch(becomeMember(id));
-
+  const btnBadge = reserved ? 'Leave Mission' : 'Join Mission';
+  const memberBadge = reserved ? 'Active Member' : 'NOT A MEMBER';
   return (
-    <tr className="display-mission-tr">
-      <th className="display-mission-bold">{name}</th>
-      <th className="display-mission-font">{description}</th>
-      <th>NOT A MEMBER</th>
-      <th><button type="button" onClick={joinMission}>Join Mission</button></th>
-    </tr>
+    <tbody>
+      <tr className="display-mission-tr">
+        <th className="display-mission-bold">{name}</th>
+        <th className="display-mission-font">{description}</th>
+        <th>{memberBadge}</th>
+        <th>
+          <button
+            type="button"
+            onClick={joinMission}
+          >
+            {btnBadge}
+          </button>
+        </th>
+      </tr>
+    </tbody>
   );
 };
 
@@ -23,6 +33,7 @@ DisplayMission.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default DisplayMission;
