923447aea869c676a7ed7182d1d24bebdc1948a5
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-23 12:21:17 +0300

    Add actions to render the rockets list

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,14 +1,53 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ROCKETS_URL } from '../../api/API';
+
 const GET_ROCKETS = 'spaceApp/rockets/GET_ROCKETS';
+const TOGGLE = 'spaceApp/rockets/TOGGLE';
 
 const initialRockets = [];
 const rocketsReducer = (state = initialRockets, action) => {
   switch (action.type) {
-    case GET_ROCKETS:
-      return null;
-
+    case `${GET_ROCKETS}/fulfilled`:
+      return action.payload;
+    case TOGGLE:
+      return state.map((rocket) => {
+        if (rocket.id !== action.payload) {
+          return rocket;
+        }
+        return { ...rocket, reserved: !rocket.reserved };
+      });
     default:
       return state;
   }
 };
+
+const transformData = (data) => {
+  const transformed = [];
+
+  data.forEach((obj) => {
+    const newObj = {
+      id: obj.id,
+      name: obj.rocket_name,
+      description: obj.description,
+      image: obj.flickr_images[0],
+      reserved: false,
+    };
+
+    transformed.push(newObj);
+  });
+
+  return transformed;
+};
+
+export const toggleReservedStatus = (id) => ({ type: TOGGLE, payload: id });
+
+export const getRockets = createAsyncThunk(GET_ROCKETS, async () => {
+  const res = await fetch(ROCKETS_URL);
+  const data = await res.json();
+
+  const transformed = transformData(data);
+
+  return transformed;
+});
 
 export default rocketsReducer;
