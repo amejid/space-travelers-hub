c7fbcfc6de30ed73b1437de41b356097f24aa7aa
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-23 12:23:23 +0300

    Render rockets data

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,3 +1,31 @@
-const Rockets = () => <div>Rockets</div>;
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRockets } from '../../redux/rockets/rockets';
+import Rocket from './Rocket';
+import styles from './Rockets.module.css';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets);
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
+  return (
+    <div className={styles.rockets}>
+      {rockets.map((rocket) => (
+        <Rocket
+          key={rocket.id}
+          id={rocket.id}
+          name={rocket.name}
+          description={rocket.description}
+          image={rocket.image}
+          reserved={rocket.reserved}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Rockets;
