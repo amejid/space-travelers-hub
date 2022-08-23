b81e85a14d19d1f70cc4799fd7078a128679140a
stephnna <stephennnabuike@gmail.com>
2022-08-23 14:48:03 +0100

    Display mission on UI

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,9 +1,10 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { GetMissionsFromApi } from '../../redux/missions/missions';
+import DisplayMission from './DisplayMissions';
 
 const Missions = () => {
- const missions =  useSelector((state) => state.missions);
+  const missions = useSelector((state) => state.missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -12,8 +13,25 @@ const Missions = () => {
   [dispatch]);
 
   return (
-    <div />
+    <div className="mission-container">
+      <hr />
+      <table className="mission-table">
+        <tr className="display-mission-tr">
+          <th className="display-mission-bold">Missions</th>
+          <th className="display-mission-bold">Description</th>
+          <th className="display-mission-bold">Status</th>
+          <th>{' '}</th>
+        </tr>
+        {missions.map((itemArr) => (
+          itemArr.map((mission) => (
+            <DisplayMission
+              name={mission.mission_name}
+              description={mission.description}
+              key={mission.mission_id}
+            />
+          ))))}
+      </table>
+    </div>
   );
 };
-
 export default Missions;
