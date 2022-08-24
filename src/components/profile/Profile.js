896900a1869264385324d9d75a886eb01cf85597
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-24 18:34:41 +0300

    Render reserved rockets on profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,9 +4,11 @@ const Profile = () => {
   const missions = useSelector((state) => state.missions);
   const reservedMissions = missions.filter((mission) => mission.reserved === true);
 
+  const rockets = useSelector((state) => state.rockets);
+  const rocketsFiltered = rockets.filter((rocket) => rocket.reserved === true);
+
   return (
     <div className="profile-table-container">
-      <hr />
       <table className="profile-table">
         <thead>
           <tr>
@@ -14,11 +16,28 @@ const Profile = () => {
           </tr>
         </thead>
         <tbody>
-          {reservedMissions.length > 0 && reservedMissions.map(({ id, name }) => (
-            <tr key={id}>
-              <td className="profile-td">{name}</td>
-            </tr>
-          ))}
+          {reservedMissions.length > 0
+            && reservedMissions.map(({ id, name }) => (
+              <tr key={id}>
+                <td className="profile-td">{name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+
+      <table className="profile-table">
+        <thead>
+          <tr>
+            <th className="profile-th">My Rockets</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rocketsFiltered.length > 0
+            && rocketsFiltered.map(({ id, name }) => (
+              <tr key={id}>
+                <td className="profile-td">{name}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
--- src/routes/Navbar.js
+++ src/routes/Navbar.js
@@ -4,39 +4,45 @@ import logo from '../assets/img/logo.png';
 import styles from './Navbar.module.css';
 
 const Navbar = () => (
-  <header className={styles.header}>
-    <div className={styles['header-left']}>
-      <img src={logo} alt="Logo" className={styles.logo} />
-      <p className={styles['header-title']}>Space Travelers Hub</p>
-    </div>
-    <nav className={styles.nav}>
-      <ul className={styles['nav-list']}>
-        <li>
-          <NavLink to="/" className={({ isActive }) => (isActive ? `${styles.active}` : undefined)}>
-            Rockets
-          </NavLink>
-        </li>
-        <li>|</li>
-        <li>
-          <NavLink
-            to="/missions"
-            className={({ isActive }) => (isActive ? `${styles.active}` : undefined)}
-          >
-            Missions
-          </NavLink>
-        </li>
-        <li>|</li>
-        <li>
-          <NavLink
-            to="/profile"
-            className={({ isActive }) => (isActive ? `${styles.active}` : undefined)}
-          >
-            My Profile
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <>
+    <header className={styles.header}>
+      <div className={styles['header-left']}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+        <p className={styles['header-title']}>Space Travelers Hub</p>
+      </div>
+      <nav className={styles.nav}>
+        <ul className={styles['nav-list']}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? `${styles.active}` : undefined)}
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li>|</li>
+          <li>
+            <NavLink
+              to="/missions"
+              className={({ isActive }) => (isActive ? `${styles.active}` : undefined)}
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li>|</li>
+          <li>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) => (isActive ? `${styles.active}` : undefined)}
+            >
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <hr />
+  </>
 );
 
 export default Navbar;
